8df1e1ff9b3ea1e60c1db9574e6383899a9ac65f
mirkone1994 <mirkochiappori@gmail.com>
2021-07-08 15:29:55 +0200

    var assigned

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//form
+var name = document.getElementById("name");
+var km = document.getElementById("km");
+var age = document.getElementById("age");
+var button = document.getElementById("validate-ticket");
+//ticket
+var passenger = document.getElementById("passenger");
+var sale = document.getElementById("sale");
+var train = document.getElementById("train");
+var nTrain = document.getElementById("ntrain");
+var price = document.getElementById("price");
