227d473dd40b9980f52cbddc4c953c861a4e5de7
mirkone1994 <mirkochiappori@gmail.com>
2021-07-08 16:19:44 +0200

    event on click done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //form
-var name = document.getElementById("name");
+var passengerName = document.getElementById("name");
 var km = document.getElementById("km");
 var age = document.getElementById("age");
 var button = document.getElementById("validate-ticket");
@@ -8,4 +8,29 @@ var passenger = document.getElementById("passenger");
 var sale = document.getElementById("sale");
 var train = document.getElementById("train");
 var nTrain = document.getElementById("ntrain");
-var price = document.getElementById("price");
+var priceDisplay = document.getElementById("price");
+//click
+button.addEventListener("click", function () {
+    var nameValue = passengerName.value;
+    console.log(nameValue);
+    var kmValue = km.value;
+    var ageValue = age.value;
+    var price = 0.21 * kmValue; 
+    var saleDisplay = "Tariffa over 18";
+    if (ageValue === "under") {
+        price *= 0.8;
+        saleDisplay = "Tariffa under 18"
+    } else if (ageValue === "Over65") {
+        price *= 0.6;
+        saleDisplay = "Tariffa Over 65"
+    }
+    price = "Euro " + price.toFixed(2);
+    var trainNumber = Math.floor(Math.random() * 8) + 1;
+
+    passenger.innerHTML = nameValue;
+    sale.innerHTML = saleDisplay;
+    train.innerHTML = trainNumber;
+    console.log(train);
+    priceDisplay.innerHTML = price;
+    console.log(price);
+});
